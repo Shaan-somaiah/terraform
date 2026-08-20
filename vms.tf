65cfc67401bfbfb55ee2b9171f909798e0f51fa7
Shaan-somaiah <shaansomaiah@gmail.com>
2026-08-20 14:27:34 +0530

    Remove dns-node2

--- vms.tf
+++ vms.tf
@@ -1,30 +1,11 @@
 locals {
   vms = {
 
-    dns-node2 = {
-      vmid        = 107
-      node        = "pve-node1"
-      clone_from  = 100 ##debian-13-small
-      cpu         = 1
-      memory      = 2048
-      bridge      = "vmbr10"
-      tags = [
-        "terraform",
-        "core"
-      ]
-      startup = {
-        order      = 1
-        up_delay   = 0
-        down_delay = 0
-      }
-
-    },
-
     nas = {
       vmid        = 2010
       node        = "pve-node1"
       clone_from  = 100
-      cpu         = 1
+      cpu         = 2
       memory      = 6144
       bridge      = "vmbr20"
       tags = [
